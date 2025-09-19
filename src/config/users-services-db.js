608fc78ee4d99c74239182e63e43bbf4d026fd87
NicolasGO21 <trucelpro7@gmail.com>
2025-09-19 19:47:05 -0300

    chore: added a gitignore

---
+++ src/config/users-services-db.js
@@ -0,0 +1,11 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // Render te da esta URL
+});
+
+export default pool;
