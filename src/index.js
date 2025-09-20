7f6f16a974882c829f9dac15096b70ced3d79e86
NicolasGO21 <trucelpro7@gmail.com>
2025-09-19 21:00:29 -0300

    feat: initial index

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// Rutas
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Users Service funcionando 🚇');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Users Service corriendo en puerto ${PORT}`));
